caf4309b50c8b2923f934eca613180a72a7ed240
prasannaelan <prasannaelan11@gmail.com>
2024-04-16 23:44:51 +0530

    prasanna code

---
+++ src/org/codings/EmployeeDetails.java
@@ -0,0 +1,26 @@
+package org.codings;
+
+public class EmployeeDetails {
+	public static void main(String[] args) {
+		int age = 10;
+		// 10 == 10t
+		if (age == 10) {
+			System.out.println("eligible1");
+			//10 == 5f
+			if (age == 5) {
+				System.out.println("eligible2");
+				
+			}
+				
+				
+			}
+		else {
+			System.out.println("not eligible");
+			
+		}
+		
+		
+	}
+
+}
+//nested if
